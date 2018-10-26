2fad1ee5a574761a5f5689488b075953eb3a074e
Ridho Bayu Prihatmoko <44189235+Rbp901@users.noreply.github.com>
2018-10-26 09:06:12 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,19 @@
+<html>
+
+<style>
+table,th,td :1px ;
+</style>
+
+<table width=80% bgcolor=white>
+
+<tr>
+<th>Company</th> <th>Contact</th> <th>Country</th>
+</tr>
+
+<tr bgcolor = green>
+<td>Alfreds Futterkiste</td> <td>Maria Andres</td> <td>Germany</td>
+</tr>
+
+</table>
+</body>
+</html>
